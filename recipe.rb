bdc21d9f77a528346ff16f7e45c461c041cd1135
saito400 <ken.s.530000@gmail.com>
2016-06-02 15:24:39 +0900

    add git installation

--- recipe.rb
+++ recipe.rb
@@ -3,10 +3,20 @@ package 'http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0
   not_if 'rpm -q nginx-release-centos-6-0.el6.ngx.noarch'
 end
 
-%w(epel-release nginx).each do |pkg|
+%w(git epel-release nginx gcc gcc-c++ openssl-devel readline-devel curl-devel expat-devel gettext-devel perl-devel zlib-devel).each do |pkg|
   package pkg do
     action :install
   end
+end
+
+execute 'install git' do
+  cwd '/usr/local/src/'
+  command <<-"EOS"
+    git clone git://git.kernel.org/pub/scm/git/git.git
+    cd git
+    make prefix=/usr/local all
+    make prefix=/usr/local install
+  EOS
 end
 
 service "nginx" do
